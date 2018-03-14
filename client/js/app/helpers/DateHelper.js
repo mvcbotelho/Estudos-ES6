d4aa2ee86ae12ef51143f96ee27cef66b349c3bb
mvcbotelho <marcus.itec@gmail.com>
2018-03-14 08:40:15 -0300

    Adição da uma regex para validação da data

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -5,10 +5,12 @@ class DateHelper {
   }
 
   static textoParaDara(texto) {
+    if(!/\d{4}-\d{2}-\d{2}/.test(texto)) 
+      throw new Error('Deve estar no formato AAAA-MM-DD')
     return new Date(...texto.split('-').map((item, indice) => item - indice % 2))
   }
 
   static dataParaTexto(data) {
-    return `${data.getDate()}/${(data.getMonth() + 1)}/${data.getFullYear()}`
+    return `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`
   }
 }
